97b8a3be3607be28298a11222e72f19b76527653
successstep <58450625+successstep@users.noreply.github.com>
2025-06-12 15:02:41 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -171,15 +171,15 @@ export const SWIMMING_COLOR = 'rgb(255,51,51)';
 
 // map tiles vendor, maptiler or mapbox or stadiamaps
 // if you want to use maptiler, set the access token in MAP_TILE_ACCESS_TOKEN
-export const MAP_TILE_VENDOR = 'maptiler';
+export const MAP_TILE_VENDOR = 'mapbox';
 
 // map tiles style name, see MAP_TILE_STYLES for more details
-export const MAP_TILE_STYLE = 'winter-dark';
+export const MAP_TILE_STYLE = 'dark-v10';
 
 // access token. you can apply a new one, it's free.
 // maptiler: Gt5R0jT8tuIYxW6sNrAg | sign up at https://cloud.maptiler.com/auth/widget
 // stadiamaps: 8a769c5a-9125-4936-bdcf-a6b90cb5d0a4 |sign up at https://client.stadiamaps.com/signup/
-export const MAP_TILE_ACCESS_TOKEN = 'TPWdaCs9qPz0mbe80yPj';
+export const MAP_TILE_ACCESS_TOKEN = 'Gt5R0jT8tuIYxW6sNrAg';
 
 export const MAP_TILE_STYLES = {
   maptiler: {
