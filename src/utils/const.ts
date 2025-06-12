b8653dcbaedfa6d68560cfb3be7e4e5176ec3d71
successstep <58450625+successstep@users.noreply.github.com>
2025-06-12 16:13:07 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -31,7 +31,7 @@ const USE_GOOGLE_ANALYTICS = false;
 const GOOGLE_ANALYTICS_TRACKING_ID = '';
 
 // styling: set to `true` if you want dash-line route
-const USE_DASH_LINE = true;
+const USE_DASH_LINE = false;
 // styling: route line opacity: [0, 1]
 const LINE_OPACITY = 0.4;
 // styling: map height
